3af26f732ba029f86e082e96b8aed97ad6ed530d
zouter <wouter.saelens@gmail.com>
2018-08-17 22:53:55 +0200

    add example of complex tree

--- data-raw/generate_help_pictures.R
+++ data-raw/generate_help_pictures.R
@@ -11,6 +11,14 @@ ggsave("inst/img/disconnected_example.png", plot = plot, width = 3, height = 3)
 
 
 ## cyclic example
-dataset <- dyntoy::generate_dataset(model = tibble(from = c("A", "A", "B", "C"), to = c("D", "B", "C", "A")) , num_cells = 1000)
+dataset <- dyntoy::generate_dataset(model = tibble(from = c("A", "A", "B", "C"), to = c("D", "B", "C", "A")), num_cells = 1000)
 plot <- plot_dimred(dataset)
 ggsave("inst/img/cyclic_example.png", plot = plot, width = 3, height = 3)
+
+
+
+
+## complex tree example
+dataset <- dyntoy::generate_dataset(model = tibble(from = c("A", "B", "B", "C", "C"), to = c("B", "C", "D", "E", "F")), num_cells = 1000)
+plot <- plot_dimred(dataset, dimred = dyndimred::dimred_landmark_mds)
+ggsave("inst/img/complex_tree_example.png", plot = plot, width = 3, height = 3)
