db4bdbe376152a51fde17b5ced65548e8d4ea038
zouter <wouter.saelens@gmail.com>
2018-09-06 12:05:52 +0200

    add trajectory type mini pictures script

---
+++ data-raw/generate_trajectory_type_pictures.R
@@ -0,0 +1,10 @@
+library(dynbenchmark)
+library(dynwrap)
+library(tidyverse)
+
+trajectory_types$id %>% map(function(trajectory_type) {
+  plot <- dynbenchmark::plot_trajectory_types(trajectory_type = trajectory_type, size = 5) + scale_x_continuous(expand = c(0.1, 0)) + scale_y_continuous(expand = c(0.3, 0))
+
+  ggsave(glue::glue("inst/img/trajectory_types/detects_{trajectory_type}.png"), width = 2, height = 1.2, bg = "transparent")
+
+})
