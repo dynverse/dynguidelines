d5a39bae56fdb1b5000f27a34fa5ccedda2b7bf2
zouter <wouter.saelens@gmail.com>
2018-09-06 10:55:46 +0200

    cleanup the methods aggr, especially the freaking vglm models

--- data-raw/methods.R
+++ data-raw/methods.R
@@ -3,4 +3,4 @@ devtools::load_all()
 
 methods_aggr <- read_rds("~/methods_aggr.rds")
 
-usethis::use_data(methods_aggr, overwrite = TRUE)
+usethis::use_data(methods_aggr, overwrite = TRUE, compress = "xz")
