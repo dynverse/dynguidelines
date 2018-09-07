cdccbdf333398f42dbb54892d835631173397fd0
zouter <wouter.saelens@gmail.com>
2018-09-07 07:06:45 +0200

    add port and launch.browser options to shiny app dynverse/dyno#19

--- R/guidelines_shiny.R
+++ R/guidelines_shiny.R
@@ -1,9 +1,26 @@
 #' @rdname guidelines
+#' @inheritParams shiny::runApp
+#' @param ... Other parameters given to [shiny::runApp()]
 #' @export
-guidelines_shiny <- function(dataset = NULL, answers = answer_questions(dataset = dataset)) {
+guidelines_shiny <- function(
+  dataset = NULL,
+  answers = answer_questions(dataset = dataset),
+  port = NULL,
+  launch.browser = TRUE,
+  host = NULL,
+  ...
+  ) {
+
   app <- shiny::shinyApp(
     shiny_ui(),
     shiny_server(answers = answers)
   )
-  shiny::runApp(app)
+
+  shiny::runApp(
+    app,
+    port = port,
+    launch.browser = launch.browser,
+    host = host,
+    ...
+  )
 }
